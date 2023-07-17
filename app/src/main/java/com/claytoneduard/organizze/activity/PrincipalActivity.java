ba6a79366b8a3dfe4230b0a0f3986a62fc1bf98e
ClaytonEduard <clayton_eduard@hotmail.com>
2023-07-17 12:14:25 -0300

    Criando mmetodo sair do app

--- app/src/main/java/com/claytoneduard/organizze/activity/PrincipalActivity.java
+++ app/src/main/java/com/claytoneduard/organizze/activity/PrincipalActivity.java
@@ -9,9 +9,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,13 +22,23 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.claytoneduard.organizze.R;
+import com.claytoneduard.organizze.config.ConfiguracaoFIrebase;
 import com.claytoneduard.organizze.databinding.ActivityPrincipalBinding;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PrincipalActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityPrincipalBinding binding;
+
+    private MaterialCalendarView calendarView;
+    private TextView textoSaudacao, textoSaldo;
+    private FirebaseAuth autenticacao;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,13 +49,46 @@ public class PrincipalActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
+        calendarView = findViewById(R.id.calendarView);
+        configuraCalendarView();
+
+        textoSaudacao = findViewById(R.id.textSaldacao);
+        textoSaldo = findViewById(R.id.textSaldo);
+
+    }
+
+    private void configuraCalendarView() {
+        CharSequence meses[] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+        calendarView.setTitleMonths(meses);
+
+        calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
+            @Override
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.bottom_nav_menu, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menuSair:
+                autenticacao = ConfiguracaoFIrebase.getFirebaseAutenticacao();
+                autenticacao.signOut();
+                // desloggar o usuario e enviar para tela principal
+                startActivity(new Intent(this, MainActivity.class));
+                // finalizar a activity princial
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void adicionarReceita(View view) {
